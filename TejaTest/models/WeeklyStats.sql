2c90563322ae8701319219d12469f4eccf7ba824
Teja Goud Kandula <kandulateja6@gmail.com>
2021-10-10 19:04:34 +0530

    2

--- TejaTest/models/WeeklyStats.sql
+++ TejaTest/models/WeeklyStats.sql
@@ -18,17 +18,17 @@
 
 {{ config(materialized='table') }}
 
-with Monthly_Cases as (
-select  SUM(New_Cases) OVER(partition by YEAR(TO_DATE(DATE)),MONTH(TO_DATE(Date))) as Monthly_Cases ,
-SUM(New_Deaths) OVER(partition by YEAR(TO_DATE(DATE)),MONTH(TO_DATE(Date))) as Monthly_Deaths ,
-SUM(New_Recovered) OVER(partition by YEAR(TO_DATE(DATE)),MONTH(TO_DATE(Date))) as Monthly_Recovered ,
-SUM(New_Active_Cases) OVER(partition by YEAR(TO_DATE(DATE)),MONTH(TO_DATE(Date))) as Monthly_Active_Cases,
-MONTH(TO_DATE(Date)) as Month,
+with Weekly_Cases as (
+select  SUM(New_Cases) OVER(partition by YEAR(TO_DATE(DATE)),WEEKOFYEAR(TO_DATE(Date))) as Weekly_Cases ,
+SUM(New_Deaths) OVER(partition by YEAR(TO_DATE(DATE)),WEEKOFYEAR(TO_DATE(Date))) as Weekly_Deaths ,
+SUM(New_Recovered) OVER(partition by YEAR(TO_DATE(DATE)),WEEKOFYEAR(TO_DATE(Date))) as Weekly_Recovered ,
+SUM(New_Active_Cases) OVER(partition by YEAR(TO_DATE(DATE)),WEEKOFYEAR(TO_DATE(Date))) as Weekly_Active_Cases,
+WEEKOFYEAR(TO_DATE(Date)) as WeekNumber,
 YEAR(TO_DATE(DATE)) as Year,
-ROW_NUMBER() OVER(Partition by YEAR(TO_DATE(DATE)), MONTH(TO_DATE(Date)) order by To_Date(Date) desc) as RNUM
+ROW_NUMBER() OVER(Partition by YEAR(TO_DATE(DATE)), WEEKOFYEAR(TO_DATE(Date)) order by To_Date(Date) desc) as RNUM
 from metrics
 where Location IN ('Indonesia')
 )
-select Monthly_Cases, Monthly_Deaths, Monthly_Recovered, Monthly_Active_Cases, Month, Year
-from Monthly_Cases where RNUM = 1
-order by Year, Month
+select Weekly_Cases, Weekly_Deaths, Weekly_Recovered, Weekly_Active_Cases, WeekNumber, Year
+from Weekly_Cases where RNUM = 1
+order by Year, WeekNumber
